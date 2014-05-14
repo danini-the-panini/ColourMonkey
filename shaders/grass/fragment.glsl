01e2c16ed5c6fa219efc89e4c7a155dc5f77f92a
Daniel Smith <jellymann@gmail.com>
2014-05-14 23:43:57 +0200

    Shadowed grass better

--- shaders/grass/fragment.glsl
+++ shaders/grass/fragment.glsl
@@ -5,18 +5,83 @@ layout (binding = 0) uniform samplerCube skymap;
 layout (binding=1) uniform sampler2D grassColour;
 layout (binding=2) uniform sampler2D grassAlpha;
 layout (binding=3) uniform sampler2D noiseMap;
+layout (binding=8) uniform sampler2D shadowMap;
 
 layout (location = 0) out vec4 colour;
 
 uniform float fog_start;
 uniform float fog_end;
 
+uniform mat4 world;
+uniform mat4 lprojection;
+uniform mat4 lview;
+
+uniform int shadowToggle;
 uniform vec3 sun;
 
+in float lightLevel;
 in vec3 w_eye;
 in vec3 g_position;
-in float lightLevel;
+in vec3 g_normal;
 in vec2 texCoord;
+
+const mat4 bias = mat4 (.5f, .0f, .0f, .0f,
+                        .0f, .5f, .0f, .0f,
+                        .0f, .0f, .5f, .0f,
+                        .5f, .5f, .5f, 1.f);
+
+float shadowed(vec2 v, float dist)
+{
+    return texture(shadowMap, v).z < dist ? 1 : 0;
+}
+
+float ia = 0.2f;
+
+float getIP(vec3 pos, vec3 norm)
+{
+    vec4 ssLightPos = bias * lprojection * lview * world * vec4(pos,1.0f);
+
+    vec3 v = normalize(w_eye-pos);
+    vec3 l = normalize(sun);
+    vec3 r = normalize(reflect(-l,norm));
+
+    float epsilon = 0.0002f;
+
+    vec2[] offsets = vec2[](
+        vec2(0,1),vec2(0,-1),vec2(1,0),vec2(-1,0),
+        vec2(1,1),vec2(1,-1),vec2(-1,-1),vec2(-1,1)
+    );
+
+    float shadow = 1.0;
+    float ip = ia;
+
+    // only shadow if sun is above horizon
+    if (sun.y > 0)
+    {
+
+        ip = lightLevel;
+
+        shadow = shadowed(ssLightPos.xy, ssLightPos.z);
+
+        for (int i = 0; i < offsets.length(); i++)
+        {
+            shadow += shadowed(ssLightPos.xy + epsilon*offsets[i], ssLightPos.z);
+        }
+        for (int i = 0; i < offsets.length(); i++)
+        {
+            shadow += shadowed(ssLightPos.xy + 2*epsilon*offsets[i], ssLightPos.z);
+        }
+
+        shadow /= offsets.length()*2+1;
+
+        bool inShadow = shadowToggle == 1 && ssLightPos.x > 0 && ssLightPos.y > 0 && ssLightPos.x < 1 && ssLightPos.y < 1;
+
+        if (inShadow)
+            ip = mix (ip, ia, shadow);
+    }
+
+    return ip * ( 1.0f );
+}
 
 void main()
 {
@@ -34,6 +99,6 @@ void main()
     if (alpha < 0.5f)
         discard;
 
-    colour = vec4(mix(sky, texture(grassColour,texCoord).rgb*lightLevel, fog_factor), 1.0f);
+    colour = vec4(mix(sky, texture(grassColour,texCoord).rgb*getIP(g_position,g_normal), fog_factor), 1.0f);
 }
 
--- shaders/grass/geometry.glsl
+++ shaders/grass/geometry.glsl
@@ -4,16 +4,10 @@ layout (triangle_strip, max_vertices=54) out;
 
 layout (binding=3) uniform sampler2D noiseMap;
 layout (binding=4) uniform sampler2D noiseMap2;
-layout (binding=8) uniform sampler2D shadowMap;
 
 uniform mat4 projection;
 uniform mat4 world;
 uniform mat4 view;
-
-uniform mat4 lprojection;
-uniform mat4 lview;
-
-uniform int shadowToggle;
 
 uniform float water_level;
 
@@ -23,15 +17,11 @@ out float lightLevel;
 out vec3 g_position;
 out vec3 w_eye;
 out vec2 texCoord;
+out vec3 g_normal;
 
 uniform float time;
 
 uniform vec3 sun;
-
-const mat4 bias = mat4 (.5f, .0f, .0f, .0f,
-                        .0f, .5f, .0f, .0f,
-                        .0f, .0f, .5f, .0f,
-                        .5f, .5f, .5f, 1.f);
 
 // some "grass configurations" I pulled out of a hat...
 
@@ -58,10 +48,7 @@ const vec2[] posOffs = vec2[](
     vec2(0,0.5f),vec2(-0.43,-0.25),vec2(0.43,-0.25)
 );
 
-float shadowed(vec2 v, float dist)
-{
-    return texture(shadowMap, v).z < dist ? 1 : 0;
-}
+
 
 float ia = 0.2f;
 float id = 0.7f;
@@ -70,8 +57,6 @@ float s = 10.0f;
 
 float getIP(vec3 pos, vec3 norm, float random)
 {
-    vec4 ssLightPos = bias * lprojection * lview * world * vec4(pos,1.0f);
-
     vec3 v = normalize(w_eye-pos);
     vec3 l = normalize(sun);
     vec3 r = normalize(reflect(-l,norm));
@@ -83,32 +68,13 @@ float getIP(vec3 pos, vec3 norm, float random)
         vec2(1,1),vec2(1,-1),vec2(-1,-1),vec2(-1,1)
     );
 
-    float shadow = 1.0;
     float ip = ia;
 
-    // only shadow if sun is above horizon
     if (sun.y > 0)
     {
 
         ip = ia + max(dot(l,norm),0)*id + pow(max(dot(r,v),0),s)*is;
 
-        shadow = shadowed(ssLightPos.xy, ssLightPos.z);
-
-        for (int i = 0; i < offsets.length(); i++)
-        {
-            shadow += shadowed(ssLightPos.xy + epsilon*offsets[i], ssLightPos.z);
-        }
-        for (int i = 0; i < offsets.length(); i++)
-        {
-            shadow += shadowed(ssLightPos.xy + 2*epsilon*offsets[i], ssLightPos.z);
-        }
-
-        shadow /= offsets.length()*2+1;
-
-        bool inShadow = shadowToggle == 1 && ssLightPos.x > 0 && ssLightPos.y > 0 && ssLightPos.x < 1 && ssLightPos.y < 1;
-
-        if (inShadow)
-            ip = mix (ip, ia, shadow);
     }
 
     return ip * ( 1.0f - random*0.1f );
@@ -124,34 +90,33 @@ void makeQuad(vec3 pos, vec2 sideA, vec2 sideB, float height, float topOffset, f
     sideA += vec2(offABit);
     sideB += vec2(offAgain);
 
-    vec3 point;
 
     lightLevel = getIP(pos,v_normal[0], jitter);
 
-    point = pos+vec3(sideA.x,-yoffset,sideA.y);
-    gl_Position = projection * view * world * vec4(point ,1.0f);
+    g_position = pos+vec3(sideA.x,-yoffset,sideA.y);
+    gl_Position = projection * view * world * vec4(g_position ,1.0f);
     texCoord = vec2(0,1);
     EmitVertex();
-    point = pos+vec3(sideA.x+topOffset,yoffset+height,sideA.y);
-    gl_Position = projection * view * world * vec4(point ,1.0f);
+    g_position = pos+vec3(sideA.x+topOffset,yoffset+height,sideA.y);
+    gl_Position = projection * view * world * vec4(g_position,1.0f);
     texCoord = vec2(0,0);
     EmitVertex();
-    point = pos+vec3(sideB.x+topOffset,yoffset+height,sideB.y);
-    gl_Position = projection * view * world * vec4(point ,1.0f);
+    g_position = pos+vec3(sideB.x+topOffset,yoffset+height,sideB.y);
+    gl_Position = projection * view * world * vec4(g_position ,1.0f);
     texCoord = vec2(1,0);
     EmitVertex();
     EndPrimitive();
 
-    point = pos+vec3(sideB.x+topOffset,yoffset+height,sideB.y);
-    gl_Position = projection * view * world * vec4(point ,1.0f);
+    g_position = pos+vec3(sideB.x+topOffset,yoffset+height,sideB.y);
+    gl_Position = projection * view * world * vec4(g_position ,1.0f);
     texCoord = vec2(1,0);
     EmitVertex();
-    point = pos+vec3(sideB.x,-yoffset,sideB.y);
-    gl_Position = projection * view * world * vec4(point ,1.0f);
+    g_position = pos+vec3(sideB.x,-yoffset,sideB.y);
+    gl_Position = projection * view * world * vec4(g_position ,1.0f);
     texCoord = vec2(1,1);
     EmitVertex();
-    point = pos+vec3(sideA.x,-yoffset,sideA.y);
-    gl_Position = projection * view * world * vec4(point ,1.0f);
+    g_position = pos+vec3(sideA.x,-yoffset,sideA.y);
+    gl_Position = projection * view * world * vec4(g_position ,1.0f);
     texCoord = vec2(0,1);
     EmitVertex();
     EndPrimitive();
@@ -160,11 +125,11 @@ void makeQuad(vec3 pos, vec2 sideA, vec2 sideB, float height, float topOffset, f
 void foo(vec3 pos)
 {
 
-    g_position = (world * vec4(pos,1.0f)).xyz;
+    vec3 pos2 = (world * vec4(pos,1.0f)).xyz;
 
-    float random = texture(noiseMap2, g_position.xz*0.05f)*2-1;
+    float random = texture(noiseMap2, pos2.xz*0.05f)*2-1;
 
-    float height = 0.45f + (texture(noiseMap, g_position.xz*0.2f)*2-1)*1f;
+    float height = 0.45f + (texture(noiseMap, pos2.xz*0.2f)*2-1)*1f;
     float topOffset = sin(time)*0.1f;
 
     if (random < 0.333f)
@@ -193,6 +158,8 @@ void foo(vec3 pos)
 void main()
 {
     if (v_normal[0].y < 0.95f) return; // no grass on cliffs
+
+    g_normal = v_normal[0];
 
     w_eye = (inverse(view) * vec4 (0, 0, 0, 1)).xyz;
 
